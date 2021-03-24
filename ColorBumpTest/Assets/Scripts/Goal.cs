0ecc9521c82e43db935346425281eab6b46357e5
ccaner37 <ccaner37@hotmail.com>
2021-03-24 21:37:49 +0300

    Color Bump 3D [Clone Game]

---
+++ ColorBumpTest/Assets/Scripts/Goal.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+	private void OnTriggerEnter(Collider other)
+	{
+		BallController ball = other.GetComponent<BallController>();
+
+		if (!ball || GameManager.singleton.GameEnded)
+		{
+			return;
+		}
+
+		GameManager.singleton.EndGame(true);
+	}
+}
